4b971fc3d4e225f1e1a44d87de2773661a5e52ee
Elliott Parker <kparker1848@gmail.com>
2021-10-06 15:30:13 -0500

    second commit

--- app.js
+++ app.js
@@ -16,24 +16,45 @@ $(document).ready(function () {
     const newDieButton = $("#newDieButton");
     newDieButton.click(() => new Die());
 
+    const sumDieButton = $("<button></button>");
+    sumDieButton.text("Add All Die");
+    $("body").append(sumDieButton)
+    sumDieButton.click(() => sumDice());
+
 
     class Die {
         constructor() {
             this.div = $("<div></div>");
-            this.value = this.roll();
+            this.roll();
             this.div.addClass("die-style");
             this.div.id = divCount;
             this.div.attr("id", this.div.id);
             $(dieContainer).append(this.div);
             dieArray.push(this);
+            console.log(dieArray)
+            this.div.click(() => this.roll());
+            this.div.dblclick(() => this.removeDie());
             divCount++;
         }
         roll() {
-            let value = randomVal(1, 7);
-            this.value = value;
+            this.value = randomVal(1, 7);
             this.div.text(`${this.value}`);
         }
+        removeDie() {
+            this.div.remove();
+            dieArray.splice(this);
+        }
 
+    };
+
+    function sumDice() {
+        let total = dieArray.reduce(addDie, 0);
+        alert(total);
+    };
+
+    let addDie = (a, b) => {
+        console.log(b)
+        return a + b.value;
     };
 
     function randomVal(min, max) {
